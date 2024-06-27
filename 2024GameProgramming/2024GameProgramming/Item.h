41319d9fa8b1325b6199040aa2bb113f28574dcf
luca3 <dudsj9782@gmail.com>
2024-06-28 00:39:18 +0900

    스테이지 클리어시 아이템 효과 유지되는 버그 해결

--- 2024GameProgramming/2024GameProgramming/Item.h
+++ 2024GameProgramming/2024GameProgramming/Item.h
@@ -5,8 +5,8 @@ class Item : public Object
 {
 public:
 	Item(POS pos, string symbol, int color);
-	void Update() override {};
-	void Render();
+	void Update() override {}; // 필요없음
+	void Render() override;
 	virtual void GetItem();
 private:
 	string _symbol;
